06452f61e04eb6aeeaea2eceb474d3388d69373b
VMahluza <mahluzavictor@gmail.com>
2025-02-11 21:52:47 +0200

    Initial Starters

---
+++ TaskTrackingBackend/src/main/java/com/TaskTrackingBackend/TaskTrackingBackend/TaskTrackingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.TaskTrackingBackend.TaskTrackingBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackingBackendApplication.class, args);
+	}
+
+}
---
+++ TaskTrackingBackend/src/test/java/com/TaskTrackingBackend/TaskTrackingBackend/TaskTrackingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskTrackingBackend.TaskTrackingBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
